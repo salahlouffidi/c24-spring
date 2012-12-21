5272453d65d29228d3a66433d06422a2580fc222
andrewelmore <andrew.elmore@incept5.com>
2012-12-21 10:02:55 +0000

    SwiftSource for use with the iO Spring Integration components

---
+++ c24-spring-core/src/main/java/biz/c24/io/spring/source/SwiftSourceFactory.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *			http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package biz.c24.io.spring.source;
+
+import java.io.InputStream;
+import java.io.Reader;
+
+import biz.c24.io.api.presentation.Source;
+import biz.c24.io.api.presentation.SwiftSource;
+
+/**
+ * @author Andrew Elmore
+ * 
+ */
+public class SwiftSourceFactory implements SourceFactory {
+
+	private String encoding;
+
+	private Boolean endOfDataRequired = null;
+
+	private Integer lookAhead = null;
+
+	private Integer lookBehind = null;
+
+	/**
+	 * 
+	 */
+	public SwiftSourceFactory() {
+	}
+
+	public Boolean getEndOfDataRequired() {
+		return endOfDataRequired;
+	}
+
+	public Integer getLookAhead() {
+		return lookAhead;
+	}
+
+	public Integer getLookBehind() {
+		return lookBehind;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * biz.c24.io.spring.source.SourceFactory#getSource(java.io.InputStream)
+	 */
+	public Source getSource(InputStream stream) {
+	    
+	    SwiftSource source = new SwiftSource(stream);
+	    
+	    configure(source);
+	    
+	    return source;
+
+	}
+
+	final protected void configure(SwiftSource source) {
+
+		// Only set the property if a value has been provided to one of the
+		// setters. If not, let the underlying Source retain the default.
+
+		if(encoding != null) {
+			source.setEncoding(encoding);
+		}
+
+		if(endOfDataRequired != null) {
+			source.setEndOfDataRequired(endOfDataRequired);
+		}
+		
+		if(lookAhead != null) {
+			source.setLookAhead(lookAhead);
+		}
+		
+		if(lookBehind != null) {
+			source.setLookBehind(lookBehind);
+		}
+
+		doConfigure(source);
+
+	}
+
+	/**
+	 * Override this to provide configuration to the source
+	 * 
+	 * @param source
+	 */
+	protected void doConfigure(SwiftSource source) {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see biz.c24.io.spring.source.SourceFactory#getSource(java.io.Reader)
+	 */
+	public Source getSource(Reader reader) {
+		SwiftSource source = new SwiftSource(reader);
+
+		configure(source);
+
+		return source;
+	}
+
+	public void setEndOfDataRequired(Boolean endOfDataRequired) {
+		this.endOfDataRequired = endOfDataRequired;
+	}
+
+	public void setLookAhead(Integer lookAhead) {
+		this.lookAhead = lookAhead;
+	}
+
+	public void setLookBehind(Integer lookBehind) {
+		this.lookBehind = lookBehind;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+}
